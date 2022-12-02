bf26772a8a80ca388999d9a20c363466a65e8261
GilianS <r0885351@student.thomasmore.be>
2022-12-02 22:09:14 +0100

    refactor: renamed files for clarity

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import Login from './components/Login.vue';
-import { RouterView } from 'vue-router'; 
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
     <div>
         <h1>Dashboard</h1>
+        <RouterView></RouterView>
     </div>
 </template>
 
