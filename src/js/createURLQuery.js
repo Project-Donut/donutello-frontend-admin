329c8fd5c8f5724774f02a2ce009a5a12748126c
GilianS <r0885351@student.thomasmore.be>
2022-12-10 15:24:23 +0100

    feat: lazy datatable with query parameters

---
+++ src/js/createURLQuery.js
@@ -0,0 +1,45 @@
+export const createURLQueryFromPrimeVue = (params) => {
+    if (!params || params === {})
+        return "";
+    console.log(JSON.parse(JSON.stringify(params)));
+    let query = [];
+
+    // Format paging
+    if (params.first === Number) query.push(`first=${params.first}`);
+    if (params.rows === Number) query.push(`rows=${params.rows}`);
+
+    // Format sorting
+    if (params.sortField) {
+        query.push(`sort=${params.sortField}`);
+        query.push(`order=${typeof params.sortOrder === Number ? params.sortOrder : 1}`);
+    }
+
+    // Format filters
+    if (!!params.filters) {
+        const filterColumns = Object.keys(params.filters);
+        let filterList = [];
+        
+        for (let column in filterColumns) {
+            // for some reason, <column> is the index within <filterColumns> here
+            // so you need to do filterColumns[column] to get the value 
+            let field = filterColumns[column];
+            let filter = params.filters[field];
+
+            console.groupCollapsed("filters");
+            console.log("column", column);
+            console.log("filterColumns", field);
+            console.log("params.filters", filter);
+            console.groupEnd();
+
+            if (filter.value !== null)
+                filterList.push(`{'${field}': '${filter.value}'}`);
+        }
+
+        if (filterList.length > 0) {
+            const filterString = `filter=[${filterList.join(',')}]`;
+            query.push(filterString);
+        }
+    }
+    
+    return "?" + query.join('&');
+}
--- src/views/app/OrdersView.vue
+++ src/views/app/OrdersView.vue
@@ -1,19 +1,22 @@
 <template>
-    <DataTable removableSort :value="orders" :paginator="true" striped-rows :rows="10" dataKey="_id" v-model:filters="filters"
-        filterDisplay="menu" responsiveLayout="scroll" :globalFilterFields="['status']">
+    <DataTable  :value="orders" :lazy="true" v-model:filters="filters" ref="dt" dataKey="_id" 
+        :totalRecords="totalRecords" :loading="loading" @page="onPage($event)" @sort="onSort($event)" @filter="onFilter($event)" filterDisplay="menu" 
+        :virtualScrollerOptions="{ lazy: true, onLazyLoad: loadData, itemSize: 5, delay: 200, showLoader: true, loading: lazyLoading, numToleratedItems: 10 }"
+        :globalFilterFields="['status']" responsiveLayout="scroll" removableSort striped-rows >
         <template #empty>
             Geen bestellingen gevonden.
         </template>
         <template #loading>
             Bestellingen aan het laden...
         </template>
-        <Column field="status" header="Status" :showFilterMenu="true" :filterMatchModeOptions="[FilterMatchMode.EQUALS]" style="min-width:12rem">
+        <Column field="status" header="Status" :showFilterMenu="true" :filterMatchModeOptions="[FilterMatchMode.EQUALS]"
+            style="min-width:12rem">
             <template #body="{ data }">
                 <span :class="'status-badge status-badge--' + data.status">{{ data.status }}</span>
             </template>
             <template #filter="{ filterModel, filterCallback }">
-                <Dropdown :ref="statusDropdown" v-model="filterModel.value" @change="e => { filterCallback(); statusFilterCallback(e) }" :options="statusOptions"
-                    placeholder="Any" class="p-column-filter" :showClear="true">
+                <Dropdown v-model="filterModel.value" @change="filterCallback"
+                    :options="statusOptions" placeholder="Any" class="p-column-filter" :showClear="true">
                     <template #value="slotProps">
                         <span :class="'status-badge status-badge--' + slotProps.value" v-if="slotProps.value">{{
                                 slotProps.value
@@ -54,13 +57,89 @@
         </Column>
     </DataTable>
 </template>
+
 <script setup>
 import { reactive, onMounted, ref } from 'vue';
 import { FilterMatchMode } from 'primevue/api';
 
 import { getOrders } from '../../api/order';
 import { formatDate } from '../../js/formatDate';
+import { createURLQueryFromPrimeVue } from '../../js/createURLQuery';
 
+// From primevue documentation
+const dt = ref();
+const loading = ref(false);
+const totalRecords = ref(0);
+const orders = ref();
+const filters = ref({
+    'status': { value: null, matchMode: FilterMatchMode.EQUALS }
+});
+const lazyParams = ref({});
+const statusOptions = ['pending', 'processing', 'shipped', 'delivered', 'cancelled'];
+// const columns = ref([
+//     {field: 'status', header: "Status"},
+//     {field: 'image', header: ""},
+//     {field: 'name', header: "Naam"},
+//     {field: 'Address', header: "Adres"},
+//     {field: 'byDate', header: "Leverdatum"},
+//     {header: "Acties"},
+// ]);
+const loadData = () => {
+    loading.value = true;
+    const urlQuery = createURLQueryFromPrimeVue(lazyParams.value);
+    getOrders(urlQuery)
+        .then(response => {
+            orders.value = response.data;
+            totalRecords.value = response.totalRecords;
+            loading.value = false;
+        });
+}
+
+let prevStatusFilter = null;
+const statusFilterCallback = e => {
+    if (prevStatusFilter === e.value) {
+        return;
+    }
+    orders.length = 0;
+    if (e.value !== null) {
+        query.value = `status=${e.value}`;
+    }
+    else {
+        query.value = '';
+    }
+    prevStatusFilter = e.value;
+}
+
+const onPage = (event) => {
+    lazyParams.value = event;
+    loadData();
+}
+
+const onFilter = () => {
+    lazyParams.value.filters = filters.value;
+    loadData();
+}
+
+const onSort = (event) => {
+    lazyParams.value = event;
+    loadData();
+} 
+
+onMounted(() => {
+    loading.value = true;
+
+    lazyParams.value = {
+        first: 0,
+        rows: dt.value.rows,
+        sortField: 'Priority',
+        sortOrder: 1,
+        filters: filters.value
+    };
+
+    loadData();
+});
+// Original Component
+/*
 const orders = reactive([]);
 const query = ref('');
 const filters = ref({
@@ -68,15 +147,14 @@ const filters = ref({
 });
 const statusOptions = ['pending', 'processing', 'shipped', 'delivered', 'cancelled'];
 
-let statusDropdown = ref(null);
-
-const loadOrders = async () => {
+const loadOrders = async (callback) => {
     const result = await getOrders(query.value);
     if (result.status === "success") {
         orders.push(...result.data);
         // sort by priority
         orders.sort((a, b) => (a.priority < b.priority) ? 1 : ((b.priority < a.priority) ? -1 : 0));
     }
+    callback(result);
 }
 
 let prevStatusFilter = null;
@@ -97,6 +175,7 @@ const statusFilterCallback = e => {
 }
 
 onMounted(loadOrders);
+*/
 </script>
 <style scoped>
 .action-container {
