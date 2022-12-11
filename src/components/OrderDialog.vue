f9566b15d2cb7fa2d9bfc2823693b86c2b5f8dd7
GilianS <r0885351@student.thomasmore.be>
2022-12-11 02:19:52 +0100

    feat: added OrderDialog component

---
+++ src/components/OrderDialog.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+let props = defineProps({
+    order: Object,
+    open: Booleans
+});
+</script>
+
+<style scoped>
+
+</style>
